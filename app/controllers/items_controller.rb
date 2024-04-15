e92b688e19f0c3fecd07f6cea167879161c081de
kkatht <kkatht@icloud.com>
2024-04-16 00:12:50 +0900

    2回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,50 @@
 class ItemsController < ApplicationController
 
     def index
+       @items = Item.all.order(created_at: :desc)
+    end
+
+    def new
+       @item = Item.new
+    end
+
+    def create
+      @item = Item.new(item_params)
+      if @item.save
+        redirect_to root_path
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def show
+      @item = Item.find(params[:id])
+    end
+
+    def edit
+      @item = Item.find(params[:id])
+      if current_user.id != @item.user_id
+        redirect_to root_path
+      end
+    end
+
+    def update
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
+        redirect_to root_path(item_params)
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+
+    def destroy
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:syohinmei, :syohincode, :suuryo, :comment, :image).merge(user_id: current_user.id)
     end
  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
 
   belongs_to :user
+  has_one_attached :image
+
+  validates :syohinmei, presence: true
+  validates :syohincode, presence: true
+  validates :suuryo, presence: true
+  validates :comment, presence: true
+  validates :image, presence: true
 
 end
